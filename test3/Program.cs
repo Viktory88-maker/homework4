0d134e2247ce396f56faa367ccd65edefdf64731
Viktoria <v1k0808@yandex.ru>
2022-12-15 22:36:02 +0300

    test

---
+++ test3/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 29.Напишите программу, которая задает массив
+// из 8 элементов и выводит их на экран
+
+Console.WriteLine("Программа задает массив из 8 элементов");
+Console.WriteLine(" ");
+
+int[]CreateArray(int size);
+{
+    int[] array = new int[size];
+    for(int i = 0; i < array.length; i++)
+    {
+    return array;         
+    }
+}
+void PrintArray(int[]array)
+{
+    for(int i = 0; i < array.Length - 1; i++)
+    {
+        Console.WriteLine(array[1] + ",");
+    }
+    Console.WriteLine(array[i] + ",");
+}
+Console.WriteLine("Сгенерированный массив:");
+PrintArray(CreateArray(8));
+
