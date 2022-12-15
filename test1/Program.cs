29594787112178124bf5e878ed9719021b8b7d9b
Viktoria <v1k0808@yandex.ru>
2022-12-15 22:23:07 +0300

    test

---
+++ test1/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 25. Напишите цикл, который принимает на вход два числа 
+//(А И В) и возводит число А в натуральную степень.
+
+Console.WriteLine("Введите число а = ");
+int a = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите число b = ");
+int b = int.Parse(Console.ReadLine()!);
+
+int Pow(int numA, int numB);
+{
+    int result = 1;
+    for(int i = 1; i <= numB; i++)
+    {
+        result *= numA;
+    }
+    return result;
+}
+Console.WriteLine ($"Результатом будет число: {Pow(a,b)}");
+
